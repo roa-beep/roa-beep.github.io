e65aba6e7f7ae93776071d5f6dad92e30ec1832e
Roa <roa.brahimi@mail.utoronto.ca>
2024-05-05 21:57:26 -0400

    v1.1.0 timeline

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <BaseLayout />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BaseLayout from './components/BaseLayout.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BaseLayout
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
