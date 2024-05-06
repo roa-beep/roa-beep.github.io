2c17941646eb59aeceb592db88016b680c887ed8
Roa <roa.brahimi@mail.utoronto.ca>
2024-05-06 12:12:22 -0400

    v1.1.3 timeline | update components and remove default assets

--- src/components/IndexPage.vue
+++ src/components/IndexPage.vue
@@ -32,6 +32,256 @@
         </div>
       </div>
     </div>
+    <div class="section-timeline">
+      <div class="container">
+        <div class="timeline_component">
+          <div class="timeline_progress">
+            <div
+              data-w-id="d5abcf1f-3370-3eea-ccfd-66f076babfdd"
+              class="timeline_progress-bar"
+            ></div>
+          </div>
+          <div
+            data-w-id="d5abcf1f-3370-3eea-ccfd-66f076babfe0"
+            class="timeline_item"
+          >
+            <div id="a" class="timeline_left">
+              <div class="timeline_date-text">Summer 2023</div>
+            </div>
+            <div id="b" class="timeline_centre">
+              <div class="timeline_circle"></div>
+            </div>
+            <div id="c" class="timeline_right">
+              <div class="timeline_image-wrapper">
+                <img
+                  src="../../public/img/Ripple.svg"
+                  loading="lazy"
+                  width="480"
+                  alt=""
+                />
+              </div>
+              <div class="margin-bottom-xlarge">
+                <div class="timeline_text">
+                  <br />At Ripple, I designed and implemented a
+                  Kubernetes-native Vector Operator, achieving real-time log
+                  routing from Kubernetes to Kafka. I introduced Vector,
+                  VectorSource, and VectorSink CRDs, establishing dynamic
+                  controllers for on-the-fly configuration. Through this
+                  project, I transformed observability and fine-tuned system
+                  monitoring, gaining unmatched experience.
+                </div>
+              </div>
+              <div class="timeline_quote-wrapper">
+                <img
+                  src="../../public/img/vec.png"
+                  loading="lazy"
+                  width="50"
+                  alt=""
+                  class="timeline_quote-image"
+                />
+                <div class="timeline_quote-text-wrapper">
+                  <p class="timeline_quote">
+                    “The Vector Operator could be a game-changer in how we
+                    handle and observe our systems, making our tasks smoother
+                    and helping us catch issues faster. Our aim? Enhanced
+                    Observability. From capturing real-time logs from Kubernetes
+                    to Kafka, to offering clear insights and quick debugging,
+                    we&#x27;re talking full coverage. With Vector as our
+                    observability main component, it ensures a streamlined
+                    process, less clutter, and a centralized tool approach. This
+                    isn&#x27;t just about altering logs; it&#x27;s about
+                    flexibility, control, and adapting quickly to our changing
+                    needs.”<br />
+                  </p>
+                  <p class="timeline_quote-title">Demo Excerpt<br /></p>
+                </div>
+              </div>
+              <br />
+              <div class="timeline_image-wrapper">
+                <img
+                  src="../../public/img/eeevector-3.png"
+                  loading="lazy"
+                  sizes="(max-width: 479px) 48px, (max-width: 767px) 64px, 36vw"
+                  alt=""
+                  width="425"
+                  srcset="
+                    ../../public/img/eeevector-3-p-500.png  500w,
+                    ../../public/img/eeevector-3-p-800.png  800w,
+                    ../../public/img/eeevector-3.png       1006w
+                  "
+                />
+              </div>
+            </div>
+          </div>
+          <div
+            data-w-id="5b7ae63b-6f00-e542-a023-96ea5c4a96e3"
+            class="timeline_item"
+          >
+            <div id="d" class="timeline_left">
+              <div class="timeline_date-text">Summer 2022</div>
+            </div>
+            <div id="e" class="timeline_centre">
+              <div class="timeline_circle"></div>
+            </div>
+            <div class="timeline_right">
+              <div class="margin-bottom-medium">
+                <div class="timeline_text">
+                  With Meta&#x27;s engineers, I built a scalable web app,
+                  optimized server resources, and automated deployment via
+                  CI/CD, gaining valuable mentorship.<br />
+                </div>
+              </div>
+              <div class="margin-bottom-xlarge">
+                <p class="text-colour-lightgrey">
+                  The MLH Fellowship, a 12-week program backed by Meta, propels
+                  aspiring engineers into the Production Engineering realm,
+                  blending software and systems engineering to deliver reliable,
+                  scalable tech services.<br />
+                </p>
+              </div>
+              <div class="timeline_image-wrapper margin-bottom-medium">
+                <img
+                  src="../../public/img/Meta.svg"
+                  loading="lazy"
+                  width="480"
+                  alt=""
+                /><img
+                  src="../../public/img/MLH.svg"
+                  loading="lazy"
+                  width="480"
+                  alt=""
+                />
+              </div>
+              <div class="margin-bottom-xlarge">
+                <div class="inline-block">
+                  <a
+                    href="https://www.youtube.com/watch?v=Em4k8hM0urY&amp;t=1695s"
+                    target="_blank"
+                    class="timeline_link w-inline-block"
+                  >
+                    <div>Featured in SREcon 2023 piece</div>
+                    <img
+                      src="../../public/img/open_in_new.svg"
+                      loading="lazy"
+                      alt=""
+                      class="link-icon"
+                    />
+                  </a>
+                </div>
+              </div>
+              <div class="timeline_quote-wrapper">
+                <img
+                  src="../../public/img/gornet_alex_200x230.jpg"
+                  loading="lazy"
+                  alt=""
+                  class="timeline_quote-image"
+                />
+                <div class="timeline_quote-text-wrapper">
+                  <p class="timeline_quote">
+                    “[...] Roa, who used her knowledge gained in the program to
+                    land an internship [...] the fellowship gave her access to
+                    resources and mentorship she needed to learn about the field
+                    and confidently pursue a role in it.”<br />
+                  </p>
+                  <p class="timeline_quote-title">
+                    Alex Gornet, Partner Success Manager at MLH<br />
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            data-w-id="cea39288-a86d-bc98-0716-192fe18bb831"
+            class="timeline_item"
+          >
+            <div id="f" class="timeline_left">
+              <div class="timeline_date-text">Also in Summer 2022</div>
+            </div>
+            <div id="g" class="timeline_centre">
+              <div class="timeline_circle"></div>
+            </div>
+            <div class="timeline_right">
+              <div class="margin-bottom-xlarge">
+                <div class="timeline_text">
+                  During my EQ Bank DevOps &amp; Cloud Software Engineering
+                  Internship, I focused on microservices infrastructure,
+                  UI-based log monitoring, streamlining app releases using
+                  Jenkins, and implementing anomaly alerts via Elasticsearch.<br />
+                </div>
+              </div>
+              <div class="timeline_image-wrapper margin-bottom-medium">
+                <img
+                  src="../../public/img/eq_appandcard.png"
+                  loading="lazy"
+                  width="480"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+          <div
+            data-w-id="0488106f-ce92-9b7b-01c3-8ad75f3f5e35"
+            class="timeline_item"
+          >
+            <div id="h" class="timeline_left">
+              <div class="timeline_date-text">Summer 2021</div>
+            </div>
+            <div id="i" class="timeline_centre">
+              <div class="timeline_circle"></div>
+            </div>
+            <div class="timeline_right">
+              <div class="margin-bottom-medium">
+                <div class="timeline_text">
+                  On a contracting gig at CIRID, I revamped UI components,
+                  boosting site performance and user experience, driving up the
+                  traffic.
+                </div>
+              </div>
+              <div class="timeline_image-wrapper"></div>
+            </div>
+          </div>
+          <div
+            data-w-id="72497fc0-4591-9daf-81b3-b917784e7930"
+            class="timeline_item"
+          >
+            <div id="j" class="timeline_left">
+              <div class="timeline_date-text">Fall 2020</div>
+            </div>
+            <div id="k" class="timeline_centre">
+              <div class="timeline_circle"></div>
+            </div>
+            <div class="timeline_right">
+              <div class="margin-bottom-medium">
+                <div class="timeline_text">
+                  I kicked off my studies at the University of Toronto, diving
+                  into Computer Science, Mathematics, and Statistics. It was
+                  here I got the tools and insights that paved the way for my
+                  tech adventures ahead.<br />
+                </div>
+              </div>
+              <div class="timeline_image-wrapper">
+                <img
+                  src="../../public/img/Utoronto_coa.svg.png"
+                  loading="lazy"
+                  width="480"
+                  sizes="(max-width: 479px) 48px, (max-width: 767px) 64px, (max-width: 991px) 36vw, 38vw"
+                  alt=""
+                  srcset="
+                    ../../public/img/Utoronto_coa.svg-p-500.png   500w,
+                    ../../public/img/Utoronto_coa.svg-p-800.png   800w,
+                    ../../public/img/Utoronto_coa.svg-p-1080.png 1080w,
+                    ../../public/img/Utoronto_coa.svg.png        1200w
+                  "
+                />
+              </div>
+            </div>
+          </div>
+          <div class="overlay-fade-bottom"></div>
+          <div class="overlay-fade-top"></div>
+        </div>
+      </div>
+    </div>
+
   </BaseLayout>
 </template>
 
